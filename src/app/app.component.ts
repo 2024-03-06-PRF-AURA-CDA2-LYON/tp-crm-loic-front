1674586d6cfb392bf9d237d58ac80c0300018eab
Loïc Marchand <nilpa018@yahoo.fr>
2024-10-03 18:25:13 +0200

    feat(FirstView): creating some components and prepare home page with boostrap CDN intégration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { HeaderComponent } from './header/header.component';
+import customersDatas from '../customerData.json';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomerListComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'crm2i';
+  customers = customersDatas;
+  title = 'CRM2i';
+  mail1 = 'f.lacaste@veolia.fr';
+  mail2 = 'guiadra@enedis.fr';
+  mail3 = 'hulk@marvel.com';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CustomerListComponent } from './customer-list/customer-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: CustomerListComponent, pathMatch: 'full' },
+    // { path: 'Customers', component: LoginComponent },
+    // { path: 'Orders', component: LoginComponent },
+];
